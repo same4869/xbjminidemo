6b5e4a8dc2072c1dd08f28bdae67ac933c032f82
xwang <xunwang@wenba100.com>
2016-08-19 15:34:46 +0800

    first commit

---
+++ app/src/main/java/com/example/xunwang/minixbj/MiniXbjMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xunwang.minixbj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiniXbjMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mini_xbj_main);
+    }
+}
